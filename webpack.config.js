e1001b15b7e77bda9028c43c48b01db2ac1336ee
anurag.sharma <anurag.sharma@tothenew.com>
2018-04-19 20:32:55 +0530

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: 'style-loader!css-loader'
+            },
+            {
+                test: /\.(graphql|gql)$/,
+                loader: 'graphql-tag/loader'
             }
 
         ]
